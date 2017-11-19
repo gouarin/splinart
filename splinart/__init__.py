4edb5c880432d34252c31598ccc8834708f596ca
gouarin <loic.gouarin@gmail.com>
2017-11-19 14:13:44 +0100

    test all

--- splinart/__init__.py
+++ splinart/__init__.py
@@ -9,3 +9,4 @@ from __future__ import print_function, division, absolute_import
 from .shapes import circle, line
 from .compute import update_img
 from .draw import save_img, show_img
+from .version import __version__
